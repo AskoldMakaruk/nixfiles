ad130de1ed522127ce546966a5fe08c63b156552
Askold Makaruk <askoldmakaruk@gmail.com>
2024-07-29 17:39:06 +0300

    typoes

--- parts/console.nix
+++ parts/console.nix
@@ -12,18 +12,18 @@ options = {
 
       shellAliases = {
         batat-upd = "sudo echo -e '\\c';sudo nixos-rebuild switch --flake $HOME/.dotfiles/flake.nix |& nom";
-        batat-edit = "nvim $HOME/.dotfiles/flake.nix"
-        batat-gc = "nix-collect-garbage --delete-older-than 7d"
-        grep = "grep --color=auto"
+        batat-edit = "nvim $HOME/.dotfiles/flake.nix";
+        batat-gc = "nix-collect-garbage --delete-older-than 7d";
+        grep = "grep --color=auto";
       };
       autosuggestions.enable = true;
-      histFIle = "$HOME/.config/zsh_history";
+      histFile = "$HOME/.config/zsh_history";
 
       setOptions = [ "INC_APPEND_HISTORY"];
 
-      syntaxHighlihting.enable = true;
+      syntaxHighlighting.enable = true;
 
-      users.defaultUserShell = pkgs.zsh;
-   }
-  }
+   };
+   users.defaultUserShell = pkgs.zsh;
+  };
 }
