1d9b21a71bbbd79205f93cc3a8f64475a91fdc67
Askold Makaruk <askoldmakaruk@gmail.com>
2024-08-18 11:07:09 +0300

    persisnace vim

--- parts/editor/config/nvim/plugins/init.lua
+++ parts/editor/config/nvim/plugins/init.lua
@@ -182,4 +182,12 @@ return {
       },
     },
   },
+-- Lua
+{
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  }
+}
 }
