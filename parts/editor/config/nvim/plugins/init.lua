bde6b9bc11b1553053158aedabec8f606cd7618a
Askold Makaruk <askoldmakaruk@gmail.com>
2024-08-19 16:52:44 +0300

    persistance nvim

--- parts/editor/config/nvim/plugins/init.lua
+++ parts/editor/config/nvim/plugins/init.lua
@@ -187,7 +187,9 @@ return {
   "folke/persistence.nvim",
   event = "BufReadPre", -- this will only start session saving when an actual file was opened
   opts = {
-    -- add any custom options here
+  dir = vim.fn.stdpath("data") .. "/sessions/", -- directory where session files are saved
+   need = 3,
+  branch = true, -- use git branch to save session
   }
 }
 }
