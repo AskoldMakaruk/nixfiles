fcf7e4b1d6fc0457035dec3a9408f07cf40eccdb
Askold Makaruk <askoldmakaruk@gmail.com>
2025-04-19 11:25:09 +0300

    openssh

--- configuration.nix
+++ configuration.nix
@@ -168,7 +168,17 @@
     blackbox-terminal
   ];
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+ services.openssh = {
+  enable = true;
+  ports = [ 22 ];
+  settings = {
+    PasswordAuthentication = false;
+    AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+    UseDns = true;
+    X11Forwarding = false;
+    PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+  };
+};
 
   system.stateVersion = "24.05"; # don't change
 
